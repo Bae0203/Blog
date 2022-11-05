0d7ad9c6a29f6eb6f5700222de4601a9278bc0f0
bae0203 <gmbae06@dgsw.hs.kr>
2022-11-05 19:43:19 +0900

    ADD :: 강의 1차
    코딩 애플 강의 UseState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState([
+    "남자 가방 추천",
+    "여자 맨투맨 추천",
+    "남녀 공용 신발 추천",
+  ]);
+  let [reco, setReco] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {title[0]}
+          <span
+            onClick={() => {
+              setReco((e) => e + 1);
+            }}
+          >
+            ❤️
+          </span>
+          {reco}
+        </h4>
+        <p>11월 4일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>11월 4일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>11월 4일 발행</p>
+      </div>
     </div>
   );
 }
