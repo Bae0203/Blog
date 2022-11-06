b15f264a40c300f7d731d937b6e331a0eaa32e42
bae0203 <gmbae06@dgsw.hs.kr>
2022-11-06 18:58:57 +0900

    ADD :: 숙제 props
    props로 자식 컴포넌트에 함수 넘기기

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ function App() {
     "여성 맨투맨 추천",
     "남녀 공용 신발 추천",
   ]);
-  const [reco, setReco] = useState(0);
+  const [reco, setReco] = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
+
+  const changeFirstTitle = () => {
+    let newArr = [...title];
+    newArr[0] = "여성 가방 추천";
+    setTitle(newArr);
+  };
 
   return (
     <div className="App">
@@ -26,16 +32,8 @@ function App() {
         가나다순정렬
       </button>
 
-      <button
-        onClick={() => {
-          let newArr = [...title];
-          newArr[0] = "여성 가방 추천";
-          setTitle(newArr);
-        }}
-      >
-        버튼
-      </button>
-      <div className="list">
+      <button onClick={changeFirstTitle}>버튼</button>
+      {/* <div className="list">
         <h4>
           {title[0]}
           <span
@@ -62,18 +60,56 @@ function App() {
           {title[2]}
         </h4>
         <p>11월 4일 발행</p>
-      </div>
-      {modal == true ? <Modal /> : null}
+      </div> */}
+
+      {/*
+       * map 함수란
+       * 1) 왼쪽 배열 자료만큼 내부코드 실행
+       * 2) return 오른쪽에 있는걸 배열로 담아줌
+       * 3) 유용한 파라미터 2개 사용가능
+       */}
+      {title.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                setModal(!modal);
+              }}
+            >
+              {title[i]}
+            </h4>
+            <span
+              onClick={() => {
+                let a = [...reco];
+                a[i] = a[i] + 1;
+                setReco(a);
+              }}
+            >
+              ❤️
+            </span>
+            {reco[i]}
+            <p>11월 4일 발행</p>
+          </div>
+        );
+      })}
+      {modal == true ? <Modal title={title} ChFT={changeFirstTitle} /> : null}
     </div>
   );
 }
 //컴포넌트를 만들땐 앞글자는 무조건 대문자!
-const Modal = () => {
+const Modal = (props) => {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.title}</h4>
       <p>날짜</p>
       <p>상세 내용</p>
+      <button
+        onClick={() => {
+          props.ChFT();
+        }}
+      >
+        버튼
+      </button>
     </div>
   );
 };
